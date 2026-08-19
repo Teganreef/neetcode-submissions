070af70e2e6b0fa227d2d8fa465bbfff0a1ad3e4
Tegan Reef <teganreef.d@gmail.com>
2026-08-19 15:00:35 -0400

    Add: insertionSort - submission-0

---
+++ Data Structures & Algorithms/insertionSort/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def insertionSort(self, pairs: List[Pair]) -> List[List[Pair]]:
+        res = []
+        for i in range(len(pairs)):
+            j = i 
+            while j > 0 and pairs[j - 1].key > pairs[j].key:
+                pairs[j], pairs[j-1] = pairs[j-1], pairs[j]
+                j -= 1
+        
+            res.append(pairs[:])
+
+        return res
